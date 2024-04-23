fb69686b8fe8d588e5ea5d0fa2f498ce9b7cff90
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2024-04-23 18:20:38 +0800

    Change name of uploaded artifact on Github release to follow the version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "v0.3.3"
+        versionName = "v0.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

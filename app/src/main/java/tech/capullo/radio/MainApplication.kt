92c08a497af3c72a471e66c52bafb5f8519e4d98
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2024-07-19 21:20:34 +0800

    Configure WorkManager to be initialized on demand: https://developer.android.com/develop/background-work/background-tasks/persistent/configuration/custom-configuration?hl=en#on-demand. This way we avoid a previous librespot process to be restarted when the app boots

--- app/src/main/java/tech/capullo/radio/MainApplication.kt
+++ app/src/main/java/tech/capullo/radio/MainApplication.kt
@@ -1,7 +1,13 @@
 package tech.capullo.radio
 
 import android.app.Application
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MainApplication : Application()
+class MainApplication : Application(), Configuration.Provider {
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+            .setMinimumLoggingLevel(android.util.Log.DEBUG)
+            .build()
+}

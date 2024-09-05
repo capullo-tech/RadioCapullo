c7ecc4249817b901d91676d2fc958c1aba6e83c4
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2024-09-05 21:35:22 +0800

    WIP for broadcaster

--- app/src/main/java/tech/capullo/radio/viewmodels/RadioBroadcasterViewModel.kt
+++ app/src/main/java/tech/capullo/radio/viewmodels/RadioBroadcasterViewModel.kt
@@ -8,20 +8,15 @@ import android.provider.Settings
 import android.util.Log
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.spotify.connectstate.Connect
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import tech.capullo.radio.data.AndroidZeroconfServer
 import xyz.gianlu.librespot.android.sink.AndroidSinkOutput
 import xyz.gianlu.librespot.core.Session
 import xyz.gianlu.librespot.player.Player
 import xyz.gianlu.librespot.player.PlayerConfiguration
-import java.io.IOException
 import java.net.NetworkInterface
-import java.net.ServerSocket
 import java.util.Collections
 import java.util.Locale
 import java.util.concurrent.Executors
@@ -31,10 +26,9 @@ import javax.inject.Inject
 class RadioBroadcasterViewModel @Inject constructor(
     @ApplicationContext private val applicationContext: Context,
 ): ViewModel() {
-    private val server = prepareLibrespotSession(getDeviceName())
     private val _hostAddresses = getInetAddresses().toMutableStateList()
-    private lateinit var nsdManager: NsdManager
-    private var serverSocket: ServerSocket? = null
+    val executorService = Executors.newSingleThreadExecutor()
+    private val TAG = "RadioBroadcasterViewModel"
 
     val hostAddresses: List<String>
         get() = _hostAddresses
@@ -59,27 +53,8 @@ class RadioBroadcasterViewModel @Inject constructor(
             Build.MODEL
         }
 
-    private fun prepareLibrespotSession(advertisingName: String): AndroidZeroconfServer {
-        // Configure the Spotify advertising session
-        val conf = Session.Configuration.Builder()
-            .setStoreCredentials(false)
-            .setCacheEnabled(false)
-            .build()
-        val builder = AndroidZeroconfServer.Builder(conf)
-            .setPreferredLocale(Locale.getDefault().language)
-            .setDeviceType(Connect.DeviceType.SMARTPHONE)
-            .setDeviceId(null)
-            .setDeviceName(advertisingName)
-        return builder.create()
-    }
-
-    private fun startServerSocket(): Int {
-        serverSocket = ServerSocket(0)
-        return serverSocket?.localPort ?: -1
-    }
-
     fun startNsdService() {
-        server.addSessionListener(object : AndroidZeroconfServer.SessionListener {
+        val sessionListener = object : AndroidZeroconfServer.SessionListener {
             val executorService = Executors.newSingleThreadExecutor()
             override fun sessionClosing(session: Session) {
                 session.close()
@@ -103,73 +78,99 @@ class RadioBroadcasterViewModel @Inject constructor(
                     )
                 )
             }
-        })
-        nsdManager = applicationContext.getSystemService(Context.NSD_SERVICE) as NsdManager
-
-        val serviceInfo = NsdServiceInfo().apply {
-            serviceName = "RadioCapullo"
-            serviceType = "_spotify-connect._tcp"
-            port = startServerSocket()
-            Log.d("NSD", "Service port: $port")
         }
 
-        startListening()
-        nsdManager.registerService(
-            serviceInfo,
-            NsdManager.PROTOCOL_DNS_SD,
-            registrationListener
+
+        executorService.execute(
+            ZeroconfServerRunnable(
+                getDeviceName(),
+                sessionListener,
+                applicationContext
+            )
         )
     }
 
-    private val registrationListener = object : NsdManager.RegistrationListener {
 
-        override fun onServiceRegistered(NsdServiceInfo: NsdServiceInfo) {
-            // Save the service name. Android may have changed it in order to
-            // resolve a conflict, so update the name you initially requested
-            // with the name Android actually used.
-            Log.d("NSD", "Service registered")
-        }
-
-        override fun onRegistrationFailed(serviceInfo: NsdServiceInfo, errorCode: Int) {
-            // Registration failed! Put debugging code here to determine why.
-            Log.d("NSD", "Registration failed")
-        }
-
-        override fun onServiceUnregistered(arg0: NsdServiceInfo) {
-            // Service has been unregistered. This only happens when you call
-            // NsdManager.unregisterService() and pass in this listener.
-            Log.d("NSD", "Service unregistered")
-        }
-
-        override fun onUnregistrationFailed(serviceInfo: NsdServiceInfo, errorCode: Int) {
-            // Unregistration failed. Put debugging code here to determine why.
-            Log.d("NSD", "Unregistration failed")
-        }
-    }
-
-    private fun startListening() {
-        serverSocket?.let { socket ->
-            viewModelScope.launch(Dispatchers.IO) {
-                try {
-                    while (true) {
-                        Log.d("NSD", "Listening for clients on port: ${socket.localPort} thread: ${Thread.currentThread().name}")
-                        val client = socket.accept()
-                        Log.d("NSD", "Client connected: ${client.inetAddress}")
-                        launch {
-                            server.handle(client)
-                        }
-                    }
-                } catch (e: IOException) {
-                    Log.e("NSD", "Error accepting client connection", e)
-                }
-            }
-        }
-    }
 
     fun stopNsdService() {
-        nsdManager.unregisterService(registrationListener)
+        //nsdManager.unregisterService(registrationListener)
     }
 
+    private class ZeroconfServerRunnable(
+        val advertisingName: String,
+        val sessionListener: AndroidZeroconfServer.SessionListener,
+        val applicationContext: Context
+    ) : Runnable {
+        override fun run() {
+            val server = prepareLibrespotSession(advertisingName)
+            server.addSessionListener(sessionListener)
+
+            val nsdManager = applicationContext.getSystemService(Context.NSD_SERVICE) as NsdManager
+
+            val serviceInfo = NsdServiceInfo().apply {
+                serviceName = "RadioCapullo"
+                serviceType = "_spotify-connect._tcp"
+                port = server.listenPort
+                Log.d("NSD", "Service port: $port")
+            }
+
+            nsdManager.registerService(
+                serviceInfo,
+                NsdManager.PROTOCOL_DNS_SD,
+                registrationListener
+            )
+
+            Runtime.getRuntime().addShutdownHook(
+                Thread {
+                    try {
+                        server.closeSession()
+                        server.close()
+                    } catch (ex: Exception) {
+                        Log.e("CAPULLO", "Error closing Zeroconf server", ex)
+                    }
+                }
+            )
+        }
+
+        private val registrationListener = object : NsdManager.RegistrationListener {
+
+            override fun onServiceRegistered(NsdServiceInfo: NsdServiceInfo) {
+                // Save the service name. Android may have changed it in order to
+                // resolve a conflict, so update the name you initially requested
+                // with the name Android actually used.
+                Log.d("NSD", "Service registered")
+            }
+
+            override fun onRegistrationFailed(serviceInfo: NsdServiceInfo, errorCode: Int) {
+                // Registration failed! Put debugging code here to determine why.
+                Log.d("NSD", "Registration failed")
+            }
+
+            override fun onServiceUnregistered(arg0: NsdServiceInfo) {
+                // Service has been unregistered. This only happens when you call
+                // NsdManager.unregisterService() and pass in this listener.
+                Log.d("NSD", "Service unregistered")
+            }
+
+            override fun onUnregistrationFailed(serviceInfo: NsdServiceInfo, errorCode: Int) {
+                // Unregistration failed. Put debugging code here to determine why.
+                Log.d("NSD", "Unregistration failed")
+            }
+        }
+        private fun prepareLibrespotSession(advertisingName: String): AndroidZeroconfServer {
+            // Configure the Spotify advertising session
+            val conf = Session.Configuration.Builder()
+                .setStoreCredentials(false)
+                .setCacheEnabled(false)
+                .build()
+            val builder = AndroidZeroconfServer.Builder(applicationContext, conf)
+                .setPreferredLocale(Locale.getDefault().language)
+                .setDeviceType(Connect.DeviceType.SPEAKER)
+                .setDeviceId(null)
+                .setDeviceName(advertisingName)
+            return builder.create()
+        }
+    }
     interface SessionChangedCallback {
         fun onPlayerReady(player: Player)
         fun onPlayerError(ex: Exception)
@@ -201,7 +202,5 @@ class RadioBroadcasterViewModel @Inject constructor(
     override fun onCleared() {
         super.onCleared()
         Log.d("ViewModel", "RadioViewModel destroyed")
-        serverSocket?.close()
-        nsdManager.unregisterService(registrationListener)
     }
 }

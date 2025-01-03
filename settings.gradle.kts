4a4d87102143ea9b7d150996dc0577c58daa501e
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2025-01-03 08:32:38 +0800

    Remove snapcast source code. Upgrade to kotlin 2.1.0 (#25)
    * Remove snapcast submodule
    
    * feat: Upgrade to kotlin 2.1.0. Upgrade hilt to support it

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,14 +10,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven {
-            name = "GithubPackages"
-            url = uri("https://maven.pkg.github.com/capullo-tech/lib-snapcast-android")
-            credentials {
-                username = providers.gradleProperty("GITHUB_USERNAME").getOrElse(System.getenv("GITHUB_USERNAME") ?: "")
-                password = providers.gradleProperty("GITHUB_TOKEN").getOrElse(System.getenv("GITHUB_TOKEN") ?: "")
-            }
-        }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 

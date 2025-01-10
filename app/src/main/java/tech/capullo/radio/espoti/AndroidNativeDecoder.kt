ad4befc42e4d41fd6d15e5d488fa3f166636a131
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2025-01-10 08:00:59 +0800

    refactor: Converted AndroidNativeDecoder to kotlin

--- app/src/main/java/tech/capullo/radio/espoti/AndroidNativeDecoder.kt
+++ app/src/main/java/tech/capullo/radio/espoti/AndroidNativeDecoder.kt
@@ -1,152 +1,162 @@
-package tech.capullo.radio.espoti;
+package tech.capullo.radio.espoti
 
-import android.media.AudioFormat;
-import android.media.MediaCodec;
-import android.media.MediaDataSource;
-import android.media.MediaExtractor;
-import android.media.MediaFormat;
-import android.os.Build;
-import android.util.Log;
+import android.media.AudioFormat
+import android.media.MediaCodec
+import android.media.MediaDataSource
+import android.media.MediaExtractor
+import android.media.MediaFormat
+import android.os.Build
+import android.util.Log
+import xyz.gianlu.librespot.player.decoders.Decoder
+import xyz.gianlu.librespot.player.decoders.SeekableInputStream
+import xyz.gianlu.librespot.player.mixing.output.OutputAudioFormat
+import java.io.IOException
+import java.io.OutputStream
+import java.util.concurrent.TimeUnit
+import kotlin.math.min
 
-import org.jetbrains.annotations.NotNull;
+class AndroidNativeDecoder(
+    audioIn: SeekableInputStream,
+    normalizationFactor: Float,
+    duration: Int,
+) : Decoder(audioIn, normalizationFactor, duration) {
+    private val buffer = ByteArray(2 * BUFFER_SIZE)
+    private val codec: MediaCodec
+    private val extractor: MediaExtractor
+    private val closeLock = Any()
+    private var presentationTime: Long = 0
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import java.util.concurrent.TimeUnit;
-
-import xyz.gianlu.librespot.player.decoders.Decoder;
-import xyz.gianlu.librespot.player.decoders.SeekableInputStream;
-import xyz.gianlu.librespot.player.mixing.output.OutputAudioFormat;
-
-public final class AndroidNativeDecoder extends Decoder {
-    private static final String TAG = AndroidNativeDecoder.class.getSimpleName();
-    private final byte[] buffer = new byte[2 * BUFFER_SIZE];
-    private final MediaCodec codec;
-    private final MediaExtractor extractor;
-    private final Object closeLock = new Object();
-    private long presentationTime = 0;
-
-    public AndroidNativeDecoder(@NotNull SeekableInputStream audioIn, float normalizationFactor, int duration) throws IOException, DecoderException {
-        super(audioIn, normalizationFactor, duration);
-
-        int start = audioIn.position();
-        extractor = new MediaExtractor();
-        extractor.setDataSource(new MediaDataSource() {
-            @Override
-            public int readAt(long position, byte[] buffer, int offset, int size) throws IOException {
-                audioIn.seek((int) position + start);
-                return audioIn.read(buffer, offset, size);
+    init {
+        val start = audioIn.position()
+        extractor = MediaExtractor()
+        extractor.setDataSource(object : MediaDataSource() {
+            @Throws(IOException::class)
+            override fun readAt(position: Long, buffer: ByteArray, offset: Int, size: Int): Int {
+                audioIn.seek(position.toInt() + start)
+                return audioIn.read(buffer, offset, size)
             }
 
-            @Override
-            public long getSize() {
-                return audioIn.size() - start;
+            override fun getSize(): Long = (audioIn.size() - start).toLong()
+
+            override fun close() {
+                audioIn.close()
             }
+        })
 
-            @Override
-            public void close() {
-                audioIn.close();
-            }
-        });
+        if (extractor.trackCount == 0) throw DecoderException("No tracks found.")
 
-        if (extractor.getTrackCount() == 0)
-            throw new DecoderException("No tracks found.");
+        extractor.selectTrack(0)
 
-        extractor.selectTrack(0);
+        val format = extractor.getTrackFormat(0)
 
-        MediaFormat format = extractor.getTrackFormat(0);
+        codec = MediaCodec.createDecoderByType(format.getString(MediaFormat.KEY_MIME)!!)
+        codec.configure(format, null, null, 0)
+        codec.start()
 
-        codec = MediaCodec.createDecoderByType(format.getString(MediaFormat.KEY_MIME));
-        codec.configure(format, null, null, 0);
-        codec.start();
-
-        int sampleSize = 16;
+        var sampleSize = 16
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            switch (format.getInteger(MediaFormat.KEY_PCM_ENCODING, AudioFormat.ENCODING_PCM_16BIT)) {
-                case AudioFormat.ENCODING_PCM_8BIT:
-                    sampleSize = 8;
-                    break;
-                case AudioFormat.ENCODING_PCM_16BIT:
-                    sampleSize = 16;
-                    break;
-                default:
-                    throw new DecoderException("Unsupported PCM encoding.");
+            sampleSize = when (
+                format.getInteger(
+                    MediaFormat.KEY_PCM_ENCODING,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                )
+            ) {
+                AudioFormat.ENCODING_PCM_8BIT -> 8
+                AudioFormat.ENCODING_PCM_16BIT -> 16
+                else -> throw DecoderException("Unsupported PCM encoding.")
             }
         }
 
-        setAudioFormat(new OutputAudioFormat(format.getInteger(MediaFormat.KEY_SAMPLE_RATE), sampleSize,
-                format.getInteger(MediaFormat.KEY_CHANNEL_COUNT), true, false));
+        audioFormat = OutputAudioFormat(
+            format.getInteger(MediaFormat.KEY_SAMPLE_RATE).toFloat(),
+            sampleSize,
+            format.getInteger(MediaFormat.KEY_CHANNEL_COUNT),
+            true,
+            false,
+        )
     }
 
-    @Override
-    protected int readInternal(@NotNull OutputStream out) throws IOException {
-        MediaCodec.BufferInfo info = new MediaCodec.BufferInfo();
+    @Throws(IOException::class)
+    override fun readInternal(out: OutputStream): Int {
+        val info = MediaCodec.BufferInfo()
 
         while (true) {
-            if (closed) return -1;
+            if (closed) return -1
 
-            synchronized (closeLock) {
-                int inputBufferId = codec.dequeueInputBuffer(-1);
+            synchronized(closeLock) {
+                val inputBufferId = codec.dequeueInputBuffer(-1)
                 if (inputBufferId >= 0) {
-                    ByteBuffer inputBuffer = codec.getInputBuffer(inputBufferId);
-                    int count = extractor.readSampleData(inputBuffer, 0);
+                    val inputBuffer = codec.getInputBuffer(inputBufferId)
+                    val count = extractor.readSampleData(inputBuffer!!, 0)
                     if (count == -1) {
                         try {
-                            codec.signalEndOfInputStream();
-                        } catch (IllegalStateException ex) {
-                            Log.e(TAG, "Failed signaling end of input stream.", ex);
-                            return -1;
+                            codec.signalEndOfInputStream()
+                        } catch (ex: IllegalStateException) {
+                            Log.e(TAG, "Failed signaling end of input stream.", ex)
+                            return -1
                         }
-                        return -1;
+                        return -1
                     }
-                    codec.queueInputBuffer(inputBufferId, inputBuffer.position(), inputBuffer.limit(), extractor.getSampleTime(), 0);
-                    extractor.advance();
+                    codec.queueInputBuffer(
+                        inputBufferId,
+                        inputBuffer.position(),
+                        inputBuffer.limit(),
+                        extractor.sampleTime,
+                        0,
+                    )
+                    extractor.advance()
                 }
 
-                int outputBufferId = codec.dequeueOutputBuffer(info, 10000);
+                val outputBufferId = codec.dequeueOutputBuffer(info, 10000)
                 if (outputBufferId >= 0) {
-                    ByteBuffer outputBuffer = codec.getOutputBuffer(outputBufferId);
+                    val outputBuffer = codec.getOutputBuffer(outputBufferId)
 
-                    while (outputBuffer.remaining() > 0) {
-                        int read = Math.min(outputBuffer.remaining(), buffer.length);
-                        outputBuffer.get(buffer, 0, read);
-                        out.write(buffer, 0, read);
+                    while (outputBuffer!!.remaining() > 0) {
+                        val read =
+                            min(outputBuffer.remaining().toDouble(), buffer.size.toDouble()).toInt()
+                        outputBuffer[buffer, 0, read]
+                        out.write(buffer, 0, read)
                     }
 
-                    codec.releaseOutputBuffer(outputBufferId, false);
-                    presentationTime = TimeUnit.MICROSECONDS.toMillis(info.presentationTimeUs);
-                    return info.size;
+                    codec.releaseOutputBuffer(outputBufferId, false)
+                    presentationTime = TimeUnit.MICROSECONDS.toMillis(info.presentationTimeUs)
+                    return info.size
                 } else if (outputBufferId == MediaCodec.INFO_TRY_AGAIN_LATER) {
-                    Log.d(TAG, "Timeout");
+                    Log.d(TAG, "Timeout")
                 } else if (outputBufferId == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED) {
-                    Log.d(TAG, "Output buffers changed");
+                    Log.d(TAG, "Output buffers changed")
                 } else if (outputBufferId == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
-                    Log.d(TAG, "Output format changed: " + codec.getOutputFormat());
+                    Log.d(TAG, "Output format changed: " + codec.outputFormat)
                 } else {
-                    Log.e(TAG, "Failed decoding: " + outputBufferId);
-                    return -1;
+                    Log.e(
+                        TAG,
+                        "Failed decoding: $outputBufferId",
+                    )
+                    return -1
                 }
             }
         }
     }
 
-    @Override
-    public void seek(int positionMs) {
-        extractor.seekTo(TimeUnit.MILLISECONDS.toMicros(positionMs), MediaExtractor.SEEK_TO_CLOSEST_SYNC);
+    override fun seek(positionMs: Int) {
+        extractor.seekTo(
+            TimeUnit.MILLISECONDS.toMicros(positionMs.toLong()),
+            MediaExtractor.SEEK_TO_CLOSEST_SYNC,
+        )
     }
 
-    @Override
-    public void close() throws IOException {
-        synchronized (closeLock) {
-            codec.release();
-            extractor.release();
-            super.close();
+    @Throws(IOException::class)
+    override fun close() {
+        synchronized(closeLock) {
+            codec.release()
+            extractor.release()
+            super.close()
         }
     }
 
-    @Override
-    public int time() {
-        return (int) presentationTime;
+    override fun time(): Int = presentationTime.toInt()
+
+    companion object {
+        private val TAG: String = AndroidNativeDecoder::class.java.simpleName
     }
 }

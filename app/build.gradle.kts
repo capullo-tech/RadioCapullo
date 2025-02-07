deaff88fe8027dfe576cee24acfd42afbe04af6c
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2025-02-07 19:12:57 +0800

    Release v0.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "v0.5.1"
+        versionName = "v0.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

db0172f1404aa3b490a8f5ea0bc5e5b520abec1c
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2025-02-14 09:21:29 +0800

    feat: Basic previews for RadioApp/MainScreen and RadioBroadcasterScreen

--- app/src/main/java/tech/capullo/radio/compose/RadioBroadcasterScreen.kt
+++ app/src/main/java/tech/capullo/radio/compose/RadioBroadcasterScreen.kt
@@ -1,7 +1,9 @@
 package tech.capullo.radio.compose
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -14,19 +16,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import tech.capullo.radio.snapcast.Client
-import tech.capullo.radio.snapcast.ClientConfig
-import tech.capullo.radio.snapcast.Host
-import tech.capullo.radio.snapcast.LastSeen
-import tech.capullo.radio.snapcast.SnapClient
-import tech.capullo.radio.snapcast.Volume
 import tech.capullo.radio.ui.theme.Typography
 import tech.capullo.radio.ui.theme.primaryBlack
 import tech.capullo.radio.ui.theme.secondaryOrange
@@ -38,7 +33,6 @@ fun RadioBroadcasterScreen(
     useDarkTheme: Boolean = false,
 ) {
     val colorScheme = if (useDarkTheme) darkColorScheme() else lightColorScheme()
-    val snapcastClients by viewModel.snapcastClients.collectAsState()
 
     MaterialTheme(
         colorScheme = colorScheme,
@@ -47,26 +41,23 @@ fun RadioBroadcasterScreen(
         RadioBroadcasterContent(
             hostAddresses = viewModel.hostAddresses,
             deviceName = viewModel.getDeviceName(),
-            snapcastClients,
         )
     }
 }
 
 @Composable
-fun RadioBroadcasterContent(
-    hostAddresses: List<String>,
-    deviceName: String,
-    snapcastClients: List<Client> = emptyList(),
-) {
-    Column(
+fun RadioBroadcasterContent(hostAddresses: List<String>, deviceName: String) {
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.DarkGray),
+        contentAlignment = Alignment.TopCenter,
     ) {
         Card(
             modifier = Modifier
                 .padding(16.dp)
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .fillMaxHeight(0.5f),
             elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
             shape = MaterialTheme.shapes.medium,
             colors = CardDefaults.cardColors(containerColor = primaryBlack),
@@ -98,8 +89,6 @@ fun RadioBroadcasterContent(
                 }
             }
         }
-
-        SnapclientList(snapcastClients)
     }
 }
 
@@ -109,89 +98,4 @@ fun PreviewRadioBroadcasterContent() {
     val hostAddresses = listOf("192.168.0.1", "0.0.0.0", "100.10.14.7")
     val deviceName = "Samsung Galaxy S21 Ultra Max"
     RadioBroadcasterContent(hostAddresses, deviceName)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewRadioBroadcasterContentWithClients() {
-    val hostAddresses = listOf("192.168.0.1", "0.0.0.0", "100.10.14.7")
-    val deviceName = "Samsung Galaxy S21 Ultra Max"
-
-    val sampleClients = listOf(
-        Client(
-            config = ClientConfig(
-                instance = 1,
-                latency = 0,
-                name = "Living Room",
-                volume = Volume(muted = false, percent = 85),
-            ),
-            connected = true,
-            host = Host(
-                arch = "x86_64",
-                ip = "192.168.1.100",
-                mac = "00:00:00:00:00:00",
-                name = "Karin's nacatambucho",
-                os = "Android 15",
-            ),
-            id = "client1",
-            lastSeen = LastSeen(sec = 1740010683, usec = 710695),
-            snapclient = SnapClient(
-                name = "Snapclient",
-                protocolVersion = 2,
-                version = "0.29.0",
-            ),
-        ),
-        Client(
-            config = ClientConfig(
-                instance = 2,
-                latency = 0,
-                name = "Kitchen",
-                volume = Volume(muted = true, percent = 50),
-            ),
-            connected = true,
-            host = Host(
-                arch = "x86_64",
-                ip = "192.168.1.101",
-                mac = "00:00:00:00:00:01",
-                name = "Pixel 3a",
-                os = "Android 15",
-            ),
-            id = "client2",
-            lastSeen = LastSeen(sec = 1740010683, usec = 710695),
-            snapclient = SnapClient(
-                name = "Snapclient",
-                protocolVersion = 2,
-                version = "0.29.0",
-            ),
-        ),
-        Client(
-            config = ClientConfig(
-                instance = 2,
-                latency = 0,
-                name = "Kitchen",
-                volume = Volume(muted = true, percent = 100),
-            ),
-            connected = true,
-            host = Host(
-                arch = "x86_64",
-                ip = "192.168.1.101",
-                mac = "00:00:00:00:00:01",
-                name = "mrG's oneplus 3T",
-                os = "Android 15",
-            ),
-            id = "client2",
-            lastSeen = LastSeen(sec = 1740010683, usec = 710695),
-            snapclient = SnapClient(
-                name = "Snapclient",
-                protocolVersion = 2,
-                version = "0.29.0",
-            ),
-        ),
-    )
-
-    RadioBroadcasterContent(
-        hostAddresses = hostAddresses,
-        deviceName = deviceName,
-        snapcastClients = sampleClients,
-    )
 }

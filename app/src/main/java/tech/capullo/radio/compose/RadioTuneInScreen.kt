a5ca4e7d030fb3b7d8f53d019965b4dc6de46f08
guerman5 <guerman5@gmail.com>
2025-03-03 11:30:42 +0800

    Adjust the layout of TuneInScreen when the keyboard appears

--- app/src/main/java/tech/capullo/radio/compose/RadioTuneInScreen.kt
+++ app/src/main/java/tech/capullo/radio/compose/RadioTuneInScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
@@ -91,7 +92,8 @@ fun RadioTuneInContent(
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(surfaceLight),
+            .background(surfaceLight)
+            .imePadding(),
         contentAlignment = Alignment.BottomEnd,
     ) {
         Card(

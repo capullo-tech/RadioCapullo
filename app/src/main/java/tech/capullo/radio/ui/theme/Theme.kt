00efb1324a79e24aa6200eb0c370dd937bb20c58
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2025-03-20 08:41:33 +0800

    fix: remove deprecated methods

--- app/src/main/java/tech/capullo/radio/ui/theme/Theme.kt
+++ app/src/main/java/tech/capullo/radio/ui/theme/Theme.kt
@@ -1,17 +1,12 @@
 package tech.capullo.radio.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 @Composable
 fun RadioTheme(
@@ -26,15 +21,6 @@ fun RadioTheme(
         }
         darkTheme -> darkColors
         else -> lightColors
-    }
-
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
     }
 
     MaterialTheme(

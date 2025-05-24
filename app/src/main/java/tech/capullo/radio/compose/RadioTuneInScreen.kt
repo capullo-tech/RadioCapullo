a7e9795141b4b26ceff66607524bc235405eb398
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2025-05-24 11:58:35 +0800

    wip: Button group for left, stereo, right

--- app/src/main/java/tech/capullo/radio/compose/RadioTuneInScreen.kt
+++ app/src/main/java/tech/capullo/radio/compose/RadioTuneInScreen.kt
@@ -1,13 +1,15 @@
 package tech.capullo.radio.compose
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
@@ -15,19 +17,36 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.TextAutoSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.PlayArrow
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonGroup
+import androidx.compose.material3.ButtonGroupDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.ToggleButton
+import androidx.compose.material3.ToggleButtonDefaults
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -62,95 +81,126 @@ fun RadioTuneInScreen(
         colorScheme = colorScheme,
         typography = Typography,
     ) {
-        RadioTuneInContent(
-            lastServerText = lastServerText,
-            isTunedIn = isTunedIn,
-            onTextChange = { newServerText ->
-                lastServerText = newServerText
-                radioTuneInModel.saveLastServerText(newServerText)
-            },
-            onTuneInClick = {
-                radioTuneInModel.startSnapclientService(lastServerText)
-                isTunedIn = true
-            },
-        )
+        Scaffold { innerPadding ->
+            RadioTuneInContent(
+                modifier = Modifier
+                    .padding(innerPadding),
+                lastServerText = lastServerText,
+                isTunedIn = isTunedIn,
+                onTextChange = { newServerText ->
+                    lastServerText = newServerText
+                    radioTuneInModel.saveLastServerText(newServerText)
+                },
+                onTuneInClick = {
+                    radioTuneInModel.startSnapclientService(lastServerText)
+                    isTunedIn = true
+                },
+            )
+        }
     }
 }
 
+val options = listOf("Left", "Stereo", "Right")
+val unCheckedIcons =
+    listOf(Icons.Outlined.MoreVert, Icons.Outlined.Notifications, Icons.Outlined.PlayArrow)
+val checkedIcons = listOf(Icons.Filled.MoreVert, Icons.Filled.Notifications, Icons.Filled.PlayArrow)
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun RadioTuneInContent(
+    modifier: Modifier = Modifier,
     lastServerText: String,
     isTunedIn: Boolean,
     onTextChange: (String) -> Unit,
     onTuneInClick: () -> Unit,
 ) {
-    // Add LaunchedEffect to listen for changes in lastServerText
-    LaunchedEffect(lastServerText) {
-        // Example: radioTuneInModel.saveLastServerText(lastServerText)
-    }
-
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(surfaceLight)
-            .imePadding(),
-        contentAlignment = Alignment.BottomEnd,
-    ) {
-        Card(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth()
-                .fillMaxHeight(0.5f),
-            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
-            shape = MaterialTheme.shapes.medium,
-            colors = CardDefaults.cardColors(containerColor = secondaryOrange),
+    Scaffold { innerPadding ->
+        Column(
+            modifier = modifier.padding(innerPadding).fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            //verticalArrangement = Arrangement.Center,
         ) {
-            Column(modifier = Modifier.padding(16.dp)) {
-                Text(
-                    text = "Tune In to another Radio:",
-                    style = Typography.bodyMedium,
-                    color = Color.Black,
-                )
+            // =====================================
+            Card(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                //.fillMaxHeight(0.9f),
+                elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+                shape = MaterialTheme.shapes.medium,
+                colors = CardDefaults.cardColors(containerColor = secondaryOrange),
+            ) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(
+                        text = "Tune In to another Radio:",
+                        style = Typography.bodyMedium,
+                        color = Color.Black,
+                    )
 
-                TextField(
-                    value = lastServerText,
-                    onValueChange = onTextChange,
-                    textStyle = Typography.titleLarge.copy(color = onSecondaryLight),
-                    placeholder = {
-                        Text(
-                            "Server IP",
-                            style =
-                            Typography
-                                .titleLarge.copy(color = onSecondaryLight.copy(alpha = 0.7f)),
-                        )
-                    },
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 8.dp)
-                        .border(
-                            width = 1.dp,
-                            color = Color.Black,
-                            shape = RoundedCornerShape(12.dp),
-                        )
-                        .clip(RoundedCornerShape(12.dp)),
-                    colors = TextFieldDefaults.colors(
-                        focusedTextColor = onSecondaryLight,
-                        unfocusedTextColor = onSecondaryLight,
-                        focusedContainerColor = surfaceLight.copy(alpha = 0.2f),
-                        unfocusedContainerColor = secondaryOrange,
-                        cursorColor = Color.Black,
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent,
-                    ),
-                )
+                    TextField(
+                        value = lastServerText,
+                        onValueChange = onTextChange,
+                        textStyle = Typography.titleLarge.copy(color = onSecondaryLight),
+                        placeholder = {
+                            Text(
+                                "Server IP",
+                                style =
+                                    Typography
+                                        .titleLarge.copy(color = onSecondaryLight.copy(alpha = 0.7f)),
+                            )
+                        },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp)
+                            .border(
+                                width = 1.dp,
+                                color = Color.Black,
+                                shape = RoundedCornerShape(12.dp),
+                            )
+                            .clip(RoundedCornerShape(12.dp)),
+                        colors = TextFieldDefaults.colors(
+                            focusedTextColor = onSecondaryLight,
+                            unfocusedTextColor = onSecondaryLight,
+                            focusedContainerColor = surfaceLight.copy(alpha = 0.2f),
+                            unfocusedContainerColor = secondaryOrange,
+                            cursorColor = Color.Black,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                        ),
+                    )
 
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight()
-                        .wrapContentSize(Alignment.Center),
-                ) {
+                    var selectedIndex by remember { mutableIntStateOf(0) }
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(ButtonGroupDefaults.ConnectedSpaceBetween),
+                    ) {
+                        val modifiers =
+                            listOf(Modifier.weight(1f), Modifier.weight(1.5f), Modifier.weight(1f))
+                        options.forEachIndexed { index, label ->
+                            ToggleButton(
+                                checked = selectedIndex == index,
+                                onCheckedChange = { selectedIndex = index },
+                                modifier = modifiers[index],
+                                shapes =
+                                    when (index) {
+                                        0 -> ButtonGroupDefaults.connectedLeadingButtonShapes()
+                                        options.lastIndex -> ButtonGroupDefaults.connectedTrailingButtonShapes()
+                                        else -> ButtonGroupDefaults.connectedMiddleButtonShapes()
+                                    },
+                                contentPadding = PaddingValues(0.dp)
+                            ) {
+                                Icon(
+                                    if (selectedIndex == index) checkedIcons[index] else unCheckedIcons[index],
+                                    contentDescription = "Localized description",
+                                )
+                                Spacer(Modifier.size(ToggleButtonDefaults.IconSpacing))
+                                Text(
+                                    text = label,
+                                )
+                            }
+                        }
+                    }
+
                     Button(
                         onClick = onTuneInClick,
                         enabled = !isTunedIn,
@@ -172,6 +222,13 @@ fun RadioTuneInContent(
             }
         }
     }
+    /*
+    // Add LaunchedEffect to listen for changes in lastServerText
+    LaunchedEffect(lastServerText) {
+        // Example: radioTuneInModel.saveLastServerText(lastServerText)
+    }
+
+     */
 }
 
 @Preview(showBackground = true)

cb67d127505de25b31a3c9ba5cfb17b22c6a0ea3
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2025-06-25 07:17:47 +0800

    release: v0.5.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 23
         targetSdk = 36
         versionCode = 1
-        versionName = "v0.5.3"
+        versionName = "v0.5.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

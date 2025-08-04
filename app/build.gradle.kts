e3fa5002a2c0b61118de8e8a59e8dfc891441d68
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2025-08-04 10:58:44 -0400

    deps: Dependencies update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
     kotlin {
         compilerOptions {
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+            freeCompilerArgs.add("-Xannotation-default-target=param-property")
         }
     }
     buildFeatures {

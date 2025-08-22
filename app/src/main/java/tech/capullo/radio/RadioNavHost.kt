c4153dc6fb18ef560b20fd6cbe7993fd7c10eb3d
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2025-08-22 10:04:58 +0800

    wip: nav3 dependencies and base setup

--- app/src/main/java/tech/capullo/radio/RadioNavHost.kt
+++ app/src/main/java/tech/capullo/radio/RadioNavHost.kt
@@ -1,54 +1,55 @@
 package tech.capullo.radio
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.navigation3.runtime.NavEntry
+import androidx.navigation3.ui.NavDisplay
 import tech.capullo.radio.ui.RadioApp
 import tech.capullo.radio.ui.RadioBroadcasterScreen
 import tech.capullo.radio.ui.RadioTuneInScreen
 import tech.capullo.radio.ui.theme.RadioTheme
 import tech.capullo.radio.ui.theme.SchemeChoice
 
+data object Home
+data object Broadcast
+data object TuneIn
+
 @Composable
 fun RadioCapulloNavHost(
-    navController: NavHostController = rememberNavController(),
-    startDestination: String = RadioDestinations.HOME_ROUTE,
 ) {
-    NavHost(
-        navController = navController,
-        startDestination = startDestination,
-    ) {
-        composable(RadioDestinations.HOME_ROUTE) {
-            RadioApp(
-                onStartBroadcastingClicked = {
-                    navController.navigate(RadioDestinations.BROADCAST_ROUTE)
-                },
-                onTuneInClicked = {
-                    navController.navigate(RadioDestinations.TUNE_IN_ROUTE)
-                },
-            )
-        }
-        composable(RadioDestinations.BROADCAST_ROUTE) {
-            RadioTheme(
-                schemeChoice = SchemeChoice.GREEN,
-            ) {
-                RadioBroadcasterScreen()
-            }
-        }
-        composable(RadioDestinations.TUNE_IN_ROUTE) {
-            RadioTheme(
-                schemeChoice = SchemeChoice.ORANGE,
-            ) {
-                RadioTuneInScreen()
-            }
-        }
-    }
-}
 
-object RadioDestinations {
-    const val HOME_ROUTE = "home"
-    const val BROADCAST_ROUTE = "broadcast"
-    const val TUNE_IN_ROUTE = "tune_in"
+    val backStack = remember { mutableStateListOf<Any>(Home) }
+
+    NavDisplay(
+        backStack = backStack,
+        entryProvider = { key ->
+            when(key) {
+                is Home -> NavEntry(key) {
+                    RadioApp(
+                        onStartBroadcastingClicked = { backStack.add(Broadcast) },
+                        onTuneInClicked = { backStack.add(TuneIn) },
+                    )
+                }
+
+                is Broadcast -> NavEntry(key) {
+                    RadioTheme(
+                        schemeChoice = SchemeChoice.GREEN,
+                    ) {
+                        RadioBroadcasterScreen()
+                    }
+                }
+
+                is TuneIn -> NavEntry(key) {
+                    RadioTheme(
+                        schemeChoice = SchemeChoice.ORANGE,
+                    ) {
+                        RadioTuneInScreen()
+                    }
+                }
+
+                else -> NavEntry(Unit) {}
+            }
+        }
+    )
 }
--- app/src/main/java/tech/capullo/radio/ui/RadioApp.kt
+++ app/src/main/java/tech/capullo/radio/ui/RadioApp.kt
@@ -6,23 +6,52 @@ import android.os.Build
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.twotone.MailOutline
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.PlainTooltip
+import androidx.compose.material3.RichTooltip
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import kotlinx.coroutines.launch
+import tech.capullo.radio.R
 import tech.capullo.radio.ui.theme.RadioTheme
 import tech.capullo.radio.ui.theme.SchemeChoice
 
@@ -42,6 +71,7 @@ fun RadioApp(onStartBroadcastingClicked: () -> Unit, onTuneInClicked: () -> Unit
                 onStartBroadcastingClicked = onStartBroadcastingClicked,
                 onTuneInClicked = onTuneInClicked,
             )
+            //PlainTooltipExample()
         } else {
             // Launch the permission request
             LaunchedEffect(multiplePermissionsState) {
@@ -57,6 +87,82 @@ fun RadioApp(onStartBroadcastingClicked: () -> Unit, onTuneInClicked: () -> Unit
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PlainTooltipExample(
+    modifier: Modifier = Modifier,
+    plainTooltipText: String = "Add to favorites"
+) {
+    val tooltipState = rememberTooltipState()
+    val scope = rememberCoroutineScope()
+    Scaffold { innerPadding ->
+        Column(
+                    modifier = Modifier
+                    .fillMaxSize()
+                .padding(innerPadding),
+        ) {
+            Text(plainTooltipText)
+            TooltipBox(
+                modifier = modifier,
+                positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
+                tooltip = {
+                    PlainTooltip { Text(plainTooltipText) }
+                },
+                state = tooltipState
+            ) {
+                IconButton(onClick = { /* Do something... */ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Favorite,
+                        contentDescription = "Add to favorites"
+                    )
+                }
+            }
+            Spacer(Modifier.requiredHeight(30.dp))
+            OutlinedButton(
+                onClick = { scope.launch { tooltipState.show() } }
+            ) {
+                Text("Display tooltip")
+            }
+
+
+
+            val tooltipState2 = rememberTooltipState(
+                isPersistent = true,
+            )
+            TooltipBox(
+                positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
+                tooltip = {
+                    RichTooltip(
+                        title = { Text(plainTooltipText) },
+                        action = {
+                            TextButton(
+                                onClick = {
+                                    scope.launch {
+                                        tooltipState2.dismiss()
+                                    }
+                                }
+                            ) { Text(plainTooltipText) }
+                        }
+                    ) { Text(plainTooltipText) }
+                },
+                state = tooltipState2
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Info,
+                    contentDescription = "Localized Description"
+                )
+            }
+            Spacer(Modifier.requiredHeight(30.dp))
+            OutlinedButton(
+                onClick = { scope.launch { tooltipState2.show() } }
+            ) {
+                Text("Display tooltip")
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun RadioMainScreen(onStartBroadcastingClicked: () -> Unit, onTuneInClicked: () -> Unit) {
     Scaffold { innerPadding ->
@@ -64,9 +170,97 @@ fun RadioMainScreen(onStartBroadcastingClicked: () -> Unit, onTuneInClicked: () 
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
+            //verticalArrangement = Arrangement.Center,
+            //horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+            Column(
+                //modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    "Radio Capullo",
+                    style = MaterialTheme.typography.displayMediumEmphasized,
+                    modifier = Modifier.padding(start = 24.dp),
+                )
+                Text(
+                    "Broadcast music to other phones",
+                    fontSize = 28.sp,
+                    modifier = Modifier.padding(start = 24.dp),
+                )
+            }
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Button(
+                    onClick = {},
+                    //contentPadding = ButtonDefaults.ButtonWithIconContentPadding,
+                ) {
+                    Icon(
+                        imageVector = Filled.Settings,
+                        contentDescription = "somm",
+                        modifier = Modifier.size(ButtonDefaults.IconSize)
+                    )
+                    Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                    Text(
+                        "RADIO-ON",
+                    )
+                }
+
+                Column(
+                    horizontalAlignment = Alignment.End
+                ) {
+                    Button(
+                        onClick = {},
+                        contentPadding = ButtonDefaults.ButtonWithIconContentPadding,
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Settings,
+                            contentDescription = "somm",
+                            modifier = Modifier.size(ButtonDefaults.IconSize)
+                        )
+                        Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                        Text(
+                            "Radio-on",
+                        )
+                    }
+
+                    val scope = rememberCoroutineScope()
+                    val plainTooltipText = "how to use Radio Capullo"
+                    val tooltipState2 = rememberTooltipState(
+                        isPersistent = true,
+                    )
+                    TooltipBox(
+                        positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
+                        tooltip = {
+                            RichTooltip(
+                                title = { Text(plainTooltipText) },
+                                action = {
+                                    TextButton(
+                                        onClick = {
+                                            scope.launch {
+                                                tooltipState2.dismiss()
+                                            }
+                                        }
+                                    ) { Text(plainTooltipText) }
+                                }
+                            ) { Text(plainTooltipText) }
+                        },
+                        state = tooltipState2
+                    ) {
+                        IconButton(
+                            onClick = { scope.launch { tooltipState2.show() } },
+                        ) {
+                            Icon(
+                                imageVector = Icons.Filled.Info,
+                                contentDescription = "Localized Description",
+                            )
+                        }
+                    }
+                }
+            }
+            /*
             RadioTheme(
                 schemeChoice = SchemeChoice.GREEN,
             ) {
@@ -107,6 +301,7 @@ fun RadioMainScreen(onStartBroadcastingClicked: () -> Unit, onTuneInClicked: () 
                     )
                 }
             }
+             */
         }
     }
 }
@@ -129,4 +324,10 @@ fun RadioAppPreview() {
             onTuneInClicked = {},
         )
     }
+}
+
+@Preview
+@Composable
+fun PP() {
+    RadioTheme { PlainTooltipExample() }
 }

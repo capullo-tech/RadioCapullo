4b5ebbc8a4883a2e3f6478ffcde92cb06dc4dcd0
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2025-10-29 14:45:15 +0800

    feat: sl4j android logger implementation for librespot dependency (via: com.gitlab.mvysny.slf4j:slf4j-handroid)

--- app/src/main/java/tech/capullo/radio/espoti/EspotiInitializer.kt
+++ app/src/main/java/tech/capullo/radio/espoti/EspotiInitializer.kt
@@ -1,16 +1,23 @@
 package tech.capullo.radio.espoti
 
 import android.content.Context
+import android.os.Build
 import androidx.startup.Initializer
+import org.slf4j.impl.HandroidLoggerAdapter
+import tech.capullo.radio.BuildConfig
 import xyz.gianlu.librespot.audio.decoders.Decoders
 import xyz.gianlu.librespot.audio.format.SuperAudioFormat
 
-class AndroidNativeDecoderInitializer : Initializer<Unit> {
+class EspotiInitializer : Initializer<Unit> {
 
     override fun create(context: Context) {
         // This initializer doesn't return anything (Unit)
         Decoders.registerDecoder(SuperAudioFormat.VORBIS, 0, AndroidNativeDecoder::class.java)
         Decoders.registerDecoder(SuperAudioFormat.MP3, 0, AndroidNativeDecoder::class.java)
+
+        HandroidLoggerAdapter.DEBUG = BuildConfig.DEBUG
+        HandroidLoggerAdapter.ANDROID_API_LEVEL = Build.VERSION.SDK_INT
+        HandroidLoggerAdapter.APP_NAME = "tech.capullo.radio"
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> {
